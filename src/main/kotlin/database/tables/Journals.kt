b77b86c5ee1403cb06d84cd655fa4a64fde8056e
JoeSheen <joe123sheen@hotmail.com>
2025-10-26 16:20:56 +0000

    Updated ID type in Journals.kt

--- src/main/kotlin/database/tables/Journals.kt
+++ src/main/kotlin/database/tables/Journals.kt
@@ -1,10 +1,10 @@
 package com.shoejs.database.tables
 
-import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 import org.jetbrains.exposed.sql.javatime.datetime
 
-object Journals : IntIdTable() {
+object Journals : LongIdTable() {
     val title = varchar(name = "title", length = 255)
     val content = text(name = "content")
     val createdAt = datetime(name = "created_at").defaultExpression(CurrentDateTime)
