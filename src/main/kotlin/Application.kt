93386d29ca050551eaef7e23d124fd5ccb5611d8
JoeSheen <joe123sheen@hotmail.com>
2025-11-02 21:25:20 +0000

    Created JwtConfig.kt, integrated my Security.kt file into the Application and updated the application.yaml

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,6 +1,8 @@
 package com.shoejs
 
+import com.shoejs.auth.JwtConfig
 import com.shoejs.plugins.configureDatabases
+import com.shoejs.plugins.configureSecurity
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
@@ -8,8 +10,15 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
+    val jwtConfig = JwtConfig(
+        realm = environment.config.property("jwt.realm").getString(),
+        secret = environment.config.property("jwt.secret").getString(),
+        audience = environment.config.property("jwt.audience").getString(),
+        domain = environment.config.property("jwt.domain").getString(),
+    )
+
     configureHTTP()
-    configureSecurity()
+    configureSecurity(jwtConfig)
     configureSerialization()
     configureDatabases()
     configureRouting()
---
+++ src/main/kotlin/auth/JwtConfig.kt
@@ -0,0 +1,8 @@
+package com.shoejs.auth
+
+data class JwtConfig(
+    val realm: String,
+    val secret: String,
+    val audience: String,
+    val domain: String,
+)
