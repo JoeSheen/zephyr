a8e01384d9cc467d6988a9cc90815359d6ac5f20
JoeSheen <joe123sheen@hotmail.com>
2025-11-24 19:25:28 +0000

    Refactored validation functions so they are more consistent and provide better error messages

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -11,8 +11,8 @@ import com.shoejs.features.tag.TagService
 import com.shoejs.features.tag.tagRoutes
 import com.shoejs.features.user.UserService
 import com.shoejs.features.user.userRoutes
-import com.shoejs.utils.isHexColor
-import com.shoejs.utils.isInvalidEmail
+import com.shoejs.utils.isValidEmail
+import com.shoejs.utils.isValidHexColor
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.requestvalidation.RequestValidation
@@ -22,12 +22,14 @@ import io.ktor.server.routing.routing
 fun Application.configureRouting(config: JwtConfig) {
     install(RequestValidation) {
         validate<RegisterRequest> { request ->
+            val (isValid, errorMessage) = isValidEmail(request.email)
+
             when {
                 request.firstName.isBlank() -> ValidationResult.Invalid("First name cannot be blank")
                 request.lastName.isBlank() -> ValidationResult.Invalid("Last name cannot be blank")
                 request.dateOfBirth.isBlank() -> ValidationResult.Invalid("Date of birth cannot be blank")
                 request.username.isBlank() -> ValidationResult.Invalid("Username cannot be blank")
-                isInvalidEmail(request.email) -> ValidationResult.Invalid("Invalid email format")
+                !isValid -> ValidationResult.Invalid(errorMessage ?: "Email validation failed")
                 request.password.isBlank() -> ValidationResult.Invalid("Password cannot be blank")
                 else -> ValidationResult.Valid
             }
@@ -40,9 +42,11 @@ fun Application.configureRouting(config: JwtConfig) {
             }
         }
         validate<TagRequest> { request ->
+            val (isValid, errorMessage) = isValidHexColor(request.hexColor)
+
             when {
                 request.name.isBlank() -> ValidationResult.Invalid("Tag name cannot be blank")
-                !isHexColor(request.hexColor) -> ValidationResult.Invalid("Invalid hex color")
+                !isValid -> ValidationResult.Invalid(errorMessage ?: "Color validation failed")
                 else -> ValidationResult.Valid
             }
         }
