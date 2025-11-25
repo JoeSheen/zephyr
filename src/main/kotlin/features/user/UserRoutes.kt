9f19f6e2795a9f9cf9b08e430b51cc84416312be
JoeSheen <joe123sheen@hotmail.com>
2025-11-25 19:49:08 +0000

    Created AuthorizationExtensions.kt and integrated it into UserRoutes.kt

--- src/main/kotlin/features/user/UserRoutes.kt
+++ src/main/kotlin/features/user/UserRoutes.kt
@@ -1,9 +1,8 @@
 package com.shoejs.features.user
 
+import com.shoejs.auth.checkUserIdentity
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.auth.authenticate
-import io.ktor.server.auth.jwt.JWTPrincipal
-import io.ktor.server.auth.principal
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
@@ -14,25 +13,14 @@ fun Route.userRoutes(userService: UserService) {
     route("/users") {
         authenticate("jwt-auth") {
             get("/{userId}") {
-                val principal = call.principal<JWTPrincipal>()!!
-                val userIdClaim = principal.payload.getClaim("userId").asLong()
-
-                val userId = call.parameters["userId"]?.toLong() ?: return@get call.respond(
-                    HttpStatusCode.BadRequest, "Path parameter 'userId' missing in request"
-                )
-
-                if (userIdClaim != userId) {
-                    return@get call.respond(
-                        HttpStatusCode.Forbidden, "You are not allowed to view this user's details"
-                    )
-                }
+                val userId = call.checkUserIdentity()
 
                 userService.getUserById(userId)?.let { userDetailsResponse ->
                     call.respond(HttpStatusCode.OK, userDetailsResponse)
-                } ?: call.respond(HttpStatusCode.NotFound, "User not found")
+                } ?: call.respond(HttpStatusCode.NotFound)
             }
             put("/{userId}") {
-                val principal = call.principal<JWTPrincipal>()!!
+                val userId = call.checkUserIdentity()
             }
         }
     }
