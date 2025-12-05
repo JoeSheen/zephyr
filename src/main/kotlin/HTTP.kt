1d5725c24d5d9316a07b414d53fc24900c97e57c
JoeSheen <joe123sheen@hotmail.com>
2025-12-05 22:11:10 +0000

    Removed commented code and unused imports from HTTP.kt (since the file is sticking around a little longer).

--- src/main/kotlin/HTTP.kt
+++ src/main/kotlin/HTTP.kt
@@ -1,25 +1,9 @@
 package com.shoejs
 
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.defaultheaders.*
-import io.ktor.server.plugins.requestvalidation.RequestValidation
-import io.ktor.server.plugins.requestvalidation.ValidationResult
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.plugins.swagger.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import java.sql.Connection
-import java.sql.DriverManager
-import org.jetbrains.exposed.sql.*
 
 fun Application.configureHTTP() {
     install(CORS) {
@@ -34,7 +18,4 @@ fun Application.configureHTTP() {
     install(DefaultHeaders) {
         header("X-Engine", "Ktor") // will send this header with each response
     }
-    /*routing {
-        swaggerUI(path = "openapi")
-    }*/
 }
