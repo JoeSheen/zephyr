d21981d21ae2b51b37ae11eeee9242782159af03
JoeSheen <joe123sheen@hotmail.com>
2025-12-06 13:31:44 +0000

    Started code clean-up on auth related routes

---
+++ src/main/kotlin/features/auth/AuthConfig.kt
@@ -0,0 +1,9 @@
+package com.shoejs.features.auth
+
+object AuthConfig {
+    const val AUTH_PATH_PREFIX = "/auth"
+    const val REGISTER_PATH_SUFFIX = "/register"
+    const val INVALID_REGISTRATION_RESPONSE = "Invalid Registration Request"
+    const val LOGIN_PATH_SUFFIX = "/login"
+    const val INVALID_LOGIN_RESPONSE = "Invalid username or password"
+}
--- src/main/kotlin/features/auth/AuthRoutes.kt
+++ src/main/kotlin/features/auth/AuthRoutes.kt
@@ -9,29 +9,22 @@ import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 
 fun Route.authRoutes(authService: AuthService, jwtService: JwtService) {
-    route("/auth") {
-        post("/register") {
-            val registerRequest = call.receive<RegisterRequest>()
-
-            val userResponse = authService.registerUser(registerRequest)
-            if (userResponse == null) {
-                return@post call.respond(HttpStatusCode.BadRequest, "Invalid User Registration Request")
-            }
-
-            val token = jwtService.generateAuthToken(userResponse.username, userResponse.id)
-            call.respond(HttpStatusCode.Created, AuthResponse(token = token, user = userResponse))
+    route(path = AuthConfig.AUTH_PATH_PREFIX) {
+        post(path = AuthConfig.REGISTER_PATH_SUFFIX) {
+            call.receive<RegisterRequest>().let { request ->
+                authService.registerUser(request)
+            }?.let { user ->
+                val token = jwtService.generateAuthToken(user.username, user.id)
+                call.respond(HttpStatusCode.Created, AuthResponse(token, user))
+            } ?: call.respond(HttpStatusCode.BadRequest, AuthConfig.INVALID_REGISTRATION_RESPONSE)
         }
-        post("/login") {
-            val loginRequest = call.receive<LoginRequest>()
-
-            val userResponse = authService.loginUser(loginRequest)
-            if (userResponse == null) {
-                return@post call.respond(HttpStatusCode.Unauthorized, "Invalid username or password")
-            }
-
-            val token = jwtService.generateAuthToken(userResponse.username, userResponse.id)
-            call.respond(HttpStatusCode.OK, AuthResponse(token = token, user = userResponse))
+        post(path = AuthConfig.LOGIN_PATH_SUFFIX) {
+            call.receive<LoginRequest>().let { request ->
+                authService.loginUser(request)
+            }?.let { user ->
+                val token = jwtService.generateAuthToken(user.username, user.id)
+                call.respond(HttpStatusCode.OK, AuthResponse(token, user))
+            } ?: call.respond(HttpStatusCode.Unauthorized, AuthConfig.INVALID_LOGIN_RESPONSE)
         }
     }
 }
-
